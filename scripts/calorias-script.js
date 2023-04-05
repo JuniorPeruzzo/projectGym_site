9b1526c3d6ea9e675732acf72b4a70c1fbc2a216
JuniorPeruzzo <77367798+JuniorPeruzzo@users.noreply.github.com>
2023-04-05 00:14:59 -0300

    first commit

---
+++ scripts/calorias-script.js
@@ -0,0 +1,8 @@
+function calculaCalorias(event) {
+  event.preventDefault();
+var genero = document.getElementById("genero").value;
+var idade = document.getElementById("idade").value;
+var altura = document.getElementById("altura").value;
+var peso = document.getElementById("peso").value;
+
+}
